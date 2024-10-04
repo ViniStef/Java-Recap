67777ce3b1d92b9f3dc6d4105fdd89141620d96f
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-04 20:04:27 -0300

    Class 58 constructors

---
+++ src/academy/devdojo/javaoneforall/javacore/Dconstructor/test/AnimeTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.javaoneforall.javacore.Dconstructor.test;
+
+import academy.devdojo.javaoneforall.javacore.Dconstructor.domain.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Akudama Drive", "TV", 12, "Finished");
+        Anime anime2 = new Anime();
+//        anime.init("Akudama Drive", "TV", 12, "Finished");
+        anime.print();
+        anime2.print();
+    }
+}
