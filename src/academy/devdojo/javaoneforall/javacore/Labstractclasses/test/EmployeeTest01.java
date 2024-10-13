8ad0d4fcddde160e86e066cfccbf07f1960e661f
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-13 10:38:00 -0300

    Class 84 abstract classes 01

---
+++ src/academy/devdojo/javaoneforall/javacore/Labstractclasses/test/EmployeeTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.javaoneforall.javacore.Labstractclasses.test;
+
+import academy.devdojo.javaoneforall.javacore.Labstractclasses.domain.Manager;
+
+public class EmployeeTest01 {
+    public static void main(String[] args) {
+        Manager manager = new Manager("Nami", 5000);
+        System.out.println(manager);
+    }
+
+
+}
