dbae8347d2d24cf04d3ef9ebede7d1202c3b29d6
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-14 12:04:03 -0300

    Class 96 exceptions02 - RuntimeException

--- src/academy/devdojo/javaoneforall/javacore/Oexception/error/test/StackOverflow01.java
+++ src/academy/devdojo/javaoneforall/javacore/Oexception/error/test/StackOverflow01.java
@@ -1,4 +1,4 @@
-package academy.devdojo.javaoneforall.Oexception.test;
+package academy.devdojo.javaoneforall.javacore.Oexception.error.test;
 
 public class StackOverflow01 {
     public static void main(String[] args) {
