069cab5a6b8faf83654419851f0bc88607518cc1
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-22 19:32:34 -0300

    Class 112 Date - utility classes

---
+++ src/academy/devdojo/javaoneforall/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.javaoneforall.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1729636133138L);
+        System.out.println(date);
+    }
+}
