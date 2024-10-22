50f11d9abf3446d5254676a0395b93a19015ea4a
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-22 16:40:18 -0300

    Class 107 Wrapper 02 - utility classes

--- src/academy/devdojo/javaoneforall/javacore/Pwrapper/test/WrapperTest01.java
+++ src/academy/devdojo/javaoneforall/javacore/Pwrapper/test/WrapperTest01.java
@@ -11,6 +11,8 @@ public class WrapperTest01 {
         char charP = 'W';
         boolean booleanP = false;
 
+        // autoboxing: java will convert the primitive type into the wrapper
+        // unboxing: java will convert the wrapper into its primitive type
         Byte byteW = 1;
         Short shortW = 1;
         Integer intW = 1;
@@ -19,5 +21,19 @@ public class WrapperTest01 {
         Double doubleW = 10D;
         Character charW = 'W';
         Boolean booleanW = false;
+
+        int i = intW; // unboxing
+        Integer.parseInt("2000");
+        Byte.parseByte("1");
+        boolean booleanValue = Boolean.parseBoolean("TruE"); // would still work
+        System.out.println(booleanValue);
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetter('9'));
+        System.out.println(Character.isLetterOrDigit('9'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.toUpperCase('!'));
+        System.out.println(Character.isLowerCase('9'));
+        System.out.println(Character.toLowerCase('9'));
+        System.out.println(Character.isAlphabetic('9'));
     }
 }
