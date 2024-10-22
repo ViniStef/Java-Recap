ce2b2f255563e502e37d33af64610bb936f2fbfb
Vinicius <145880205+ViniStef@users.noreply.github.com>
2024-10-22 20:41:02 -0300

    Class 115 Date Internationalization with Locale - utility classes

---
+++ src/academy/devdojo/javaoneforall/javacore/Rdates/test/LocaleTest01.java
@@ -0,0 +1,31 @@
+package academy.devdojo.javaoneforall.javacore.Rdates.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        // ISO 639, ISO 3166
+        // pt-BR, pt-PT
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeSwitzerland = new Locale("it", "CH");
+        Locale localeIndia = new Locale("hi", "IN");
+        Locale localeJapan = new Locale("ja", "JP");
+        Locale localeNetherlands = new Locale("nl", "NL");
+        Calendar calendar = Calendar.getInstance();
+        DateFormat dateFormat1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat dateFormat2 = DateFormat.getDateInstance(DateFormat.FULL, localeSwitzerland);
+        DateFormat dateFormat3 = DateFormat.getDateInstance(DateFormat.FULL, localeIndia);
+        DateFormat dateFormat4 = DateFormat.getDateInstance(DateFormat.FULL, localeJapan);
+        DateFormat dateFormat5 = DateFormat.getDateInstance(DateFormat.FULL, localeNetherlands);
+        System.out.println(dateFormat1.format(calendar.getTime()));
+        System.out.println(dateFormat2.format(calendar.getTime()));
+        System.out.println(dateFormat3.format(calendar.getTime()));
+        System.out.println(dateFormat4.format(calendar.getTime()));
+        System.out.println(dateFormat5.format(calendar.getTime()));
+
+        System.out.println(localeItaly.getDisplayCountry(localeJapan));
+        System.out.println(localeItaly.getDisplayLanguage(localeJapan));
+    }
+}
