f5d8a18d0eecc53aba4192681068834d1b0b238a
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-01-30 09:51:14 -0300

    Class 177 NavigableSet,TreeSet 02 - Collection 17

--- src/academy/devdojo/javaoneforall/javacore/Ycollection/test/NavigableSetTest01.java
+++ src/academy/devdojo/javaoneforall/javacore/Ycollection/test/NavigableSetTest01.java
@@ -14,6 +14,13 @@ class SmartphoneNameComparator implements Comparator<Smartphone> {
     }
 }
 
+class MangaPriceComparator implements Comparator<Manga> {
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPrice(), o2.getPrice());
+    }
+}
+
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         // It keeps the order of insertion
@@ -23,19 +30,35 @@ public class NavigableSetTest01 {
         set.add(new Smartphone("111", "LG"));
         System.out.println(set);
 
-        NavigableSet<Manga> mangas = new TreeSet<>();
-        mangas.add(new Manga(5L, "Pokemon", 19.99));
-        mangas.add(new Manga(3L, "Hellsing ultimate", 59.99));
-        mangas.add(new Manga(1L, "Berserk", 29.99));
-        mangas.add(new Manga(2L, "Attack on Titan", 39.99));
-        mangas.add(new Manga(4L, "Dragon Ball Z", 89.99));
-        mangas.add(new Manga(10L, "Slime", 1.99));
-        mangas.add(new Manga(10L, "Slime", 2.99));
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPriceComparator());
+        mangas.add(new Manga(5L, "Pokemon", 19.99, 0));
+        mangas.add(new Manga(3L, "Hellsing ultimate", 59.99, 5));
+        mangas.add(new Manga(1L, "Berserk", 29.99,0));
+        mangas.add(new Manga(2L, "Attack on Titan", 39.99, 8));
+        mangas.add(new Manga(4L, "Dragon Ball Z", 89.99, 0));
+        mangas.add(new Manga(10L, "Slime", 1.99, 0));
 
+        // mangas.descendingSet()
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
 
+        Manga yuyuHakusho = new Manga(11L, "Yuyu Hakusho", 1.99, 0);
+        // lower -> first lower
+        // floor -> first lower or equal
+        // higher -> first higher
+        // ceiling -> first higher or equal
+        System.out.println("---------------");
+        System.out.println(mangas.lower(yuyuHakusho));
+        System.out.println(mangas.floor(yuyuHakusho));
+        System.out.println(mangas.higher(yuyuHakusho));
+        System.out.println(mangas.ceiling(yuyuHakusho));
+
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollFirst()); // retrieve and remove the first one from the collection
+        System.out.println(mangas.size());
+        System.out.println(mangas.pollLast()); // retrieve and remove the last one from the collection
+        System.out.println(mangas.size());
 
     }
 }
