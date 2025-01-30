86b47646766222ef8eca324a19427d1acaa778b3
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-01-30 13:54:20 -0300

    Class 183 Introduction - Generics 01

---
+++ src/academy/devdojo/javaoneforall/javacore/Zgenerics/test/GenericsTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.javaoneforall.javacore.Zgenerics.test;
+
+import academy.devdojo.javaoneforall.javacore.Ycollection.domain.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+        //Type erasure
+        List<String> list = new ArrayList<>();
+        list.add("Midoriya");
+
+        for (String o: list) {
+            System.out.println(o);
+        }
+
+        add(list, new Customer("Jiraya"));
+
+    }
+
+    private static void add(List anyList, Customer c) {
+        anyList.add(c);
+    }
+}
