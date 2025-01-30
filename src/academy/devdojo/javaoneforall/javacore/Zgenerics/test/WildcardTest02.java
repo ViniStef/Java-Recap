732d966f943e62d12ec6ca1495d38715138d2a56
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-01-30 14:30:03 -0300

    Class 185 Wildcard 02 - Generics 03

--- src/academy/devdojo/javaoneforall/javacore/Zgenerics/test/WildcardTest02.java
+++ src/academy/devdojo/javaoneforall/javacore/Zgenerics/test/WildcardTest02.java
@@ -8,13 +8,28 @@ public class WildcardTest02 {
         List<Dog> dogs = List.of(new Dog(), new Dog());
         List<Cat> cats = List.of(new Cat(), new Cat());
         List<Animal> animals = new ArrayList<>(2);
-        printConsult(animals);
+        printConsult(dogs);
+        printConsult(cats);
+        printConsultDog(animals);
     }
 
-    private static void printConsult(List<Animal> animals) {
+    private static void printConsult(List<? extends Animal> animals) {
         System.out.println("-----------");
         for (Animal animal: animals) {
             animal.consult();
+        }
+    }
+
+    private static void printConsultDog(List<? super Animal> animals) {
+        System.out.println("-----------");
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        for (Object obj : animals) {
+            if (obj instanceof Dog d) {
+                d.consult();
+            }
         }
     }
 }
