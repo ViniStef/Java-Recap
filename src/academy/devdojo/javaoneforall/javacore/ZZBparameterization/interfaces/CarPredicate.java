145940e37cd9f4794779bd5bda9e23180ee39135
Vinicius <145880205+ViniStef@users.noreply.github.com>
2025-01-31 09:54:08 -0300

    Class 195 Predicate - Lambdas 01

--- src/academy/devdojo/javaoneforall/javacore/ZZBparameterization/interfaces/CarPredicate.java
+++ src/academy/devdojo/javaoneforall/javacore/ZZBparameterization/interfaces/CarPredicate.java
@@ -2,6 +2,14 @@ package academy.devdojo.javaoneforall.javacore.ZZBparameterization.interfaces;
 
 import academy.devdojo.javaoneforall.javacore.ZZBparameterization.domain.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
     boolean test(Car car);
 }
+
+// (Parameters) -> <expression>
+// (Car x) -> x.getColor().equals("green");
+// () -> System.out.println("lalala");
+
+// Lambda expressions can only work with functional interfaces (interfaces that have only one abstract method) and the
+// abstract method tells you everything you need to know about your lambda expression.
